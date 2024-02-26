888d705cd1deb0fb2a48cc3cf25729c3528f4139
Lemon Sensei <inthad.yu@gmail.com>
2024-02-26 17:25:09 +0700

    Route configs.
    เพิ่มการ Navigation แบบ Declarative โดยใช้แพคเกจ Go_Router

---
+++ lib/configs/routeConfig.dart
@@ -0,0 +1,31 @@
+import 'package:go_router/go_router.dart';
+import 'package:lemonsensei_home/screens/accomplishments.dart';
+import 'package:lemonsensei_home/screens/articles.dart';
+import 'package:lemonsensei_home/screens/contact.dart';
+import 'package:lemonsensei_home/screens/home.dart';
+import 'package:lemonsensei_home/screens/resume.dart';
+
+final router = GoRouter(
+  routes: [
+    GoRoute(
+      path: "/",
+      builder: (context, state) => const HomePage(),
+    ),
+    GoRoute(
+      path: "/articles",
+      builder: (context, state) => const ArticlePage(),
+    ),
+    GoRoute(
+      path: "/accomplishments",
+      builder: (context, state) => const AccomplishmentsPage(),
+    ),
+    GoRoute(
+      path: "/resume",
+      builder: (context, state) => const ResumePage(),
+    ),
+    GoRoute(
+      path: "/contact",
+      builder: (context, state) => const ContactPage(),
+    ),
+  ],
+);
