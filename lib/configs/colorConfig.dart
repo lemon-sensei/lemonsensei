2016229cff53759af875943c2711c47df000177c
Lemon Sensei <inthad.yu@gmail.com>
2024-02-26 17:28:55 +0700

    Text and color configs.
    เพิ่มการกำหนดรูปแบบของสีและรูปแบบอักษรที่ใช้ในเว็บ

---
+++ lib/configs/colorConfig.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Color seedColor = const Color.fromARGB(
+  255,
+  252,
+  227,
+  138,
+);
+
+ColorScheme appColorTheme = ColorScheme.fromSeed(
+  seedColor: seedColor,
+);
---
+++ lib/configs/textConfig.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+TextTheme appTextTheme = TextTheme(
+  displayLarge: GoogleFonts.prompt(),
+  displayMedium: GoogleFonts.prompt(),
+  displaySmall: GoogleFonts.prompt(),
+  headlineLarge: GoogleFonts.prompt(),
+  headlineMedium: GoogleFonts.prompt(),
+  headlineSmall: GoogleFonts.prompt(),
+  titleLarge: GoogleFonts.prompt(),
+  titleMedium: GoogleFonts.prompt(),
+  titleSmall: GoogleFonts.prompt(),
+  bodyLarge: GoogleFonts.prompt(),
+  bodyMedium: GoogleFonts.prompt(),
+  bodySmall: GoogleFonts.prompt(),
+  labelLarge: GoogleFonts.prompt(),
+  labelMedium: GoogleFonts.prompt(),
+  labelSmall: GoogleFonts.prompt(),
+);
