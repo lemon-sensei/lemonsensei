68ff84e682f9501c0c4568766e0e39ba7f09aecc
Lemon Sensei <inthad.yu@gmail.com>
2024-03-03 14:00:41 +0700

    Home - Multi-language.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -44,30 +45,26 @@ class HomePage extends StatelessWidget {
               ),
               const SizedBox(height: 30),
               Text(
-                "I'm Inthad",
+                "home.name".tr(),
                 style: Theme.of(context).textTheme.displayLarge,
               ),
               const SizedBox(height: 10),
               Text(
-                'A Technical Software Developer',
+                "home.position".tr(),
                 style: Theme.of(context).textTheme.titleLarge,
               ),
               const SizedBox(height: 30),
               Divider(
                 thickness: 3,
-                color: Colors.black,
-                indent: MediaQuery.of(context).size.width * 0.4,
-                endIndent: MediaQuery.of(context).size.width * 0.4,
+                indent: screenWidth * 0.45,
+                endIndent: screenWidth * 0.45,
               ),
               const SizedBox(height: 30),
-              const TextDescription(
-                  text:
-                      "Who working in the most burning sensation atmosphere in the world at Bangkok, Thailand. I am passionate about creating beautiful and functional mobile applications using Flutter. Feel free to explore!"),
-              const SizedBox(height: 50),
-              const TextTitle(text: 'Favorite Skills'),
+              TextDescription(text: "home.text.text-1".tr()),
+              TextTitle(text: "home.skill-title".tr()),
               const SizedBox(height: 30),
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
                 child: SizedBox(
                   width: 600,
                   child: Wrap(
@@ -75,38 +72,36 @@ class HomePage extends StatelessWidget {
                     runSpacing: 10,
                     alignment: WrapAlignment.center,
                     children: [
-                      HomeSkill(name: 'Flutter'),
-                      HomeSkill(name: 'Dart'),
-                      HomeSkill(name: 'Python'),
+                      HomeSkill(name: "Flutter"),
+                      HomeSkill(name: "home.skill.skill-dart".tr()),
+                      HomeSkill(name: "home.skill.skill-python".tr()),
                       HomeSkill(name: 'HTML'),
                       HomeSkill(name: 'CSS'),
-                      HomeSkill(name: 'JavaScript'),
-                      HomeSkill(name: 'Firebase'),
+                      HomeSkill(name: "home.skill.skill-js".tr()),
                       HomeSkill(name: 'SvelteKit'),
-                      HomeSkill(name: 'Unity3D'),
+                      HomeSkill(name: "home.skill.skill-unity".tr()),
                       HomeSkill(name: 'C#'),
-                      HomeSkill(name: 'Google Cloud'),
-                      HomeSkill(name: 'Data Engineer'),
-                      HomeSkill(name: 'Data Analytic'),
-                      HomeSkill(name: 'Data Visualization'),
+                      HomeSkill(name: 'Firebase'),
+                      HomeSkill(name: "home.skill.skill-gcloud".tr()),
+                      HomeSkill(name: 'Netlify'),
+                      HomeSkill(name: "home.skill.skill-spreadsheets".tr()),
+                      HomeSkill(name: "home.skill.skill-de".tr()),
+                      HomeSkill(name: "home.skill.skill-dc".tr()),
+                      HomeSkill(name: "home.skill.skill-dataValid".tr()),
+                      HomeSkill(name: "home.skill.skill-da".tr()),
+                      HomeSkill(name: "home.skill.skill-dataVis".tr()),
                     ],
                   ),
                 ),
               ),
               const SizedBox(height: 50),
-              const Testimonial(path: 'assets/images/galleries/'),
+              TextDescription(text: "home.text.text-2".tr()),
+              const SizedBox(height: 30),
+              TextDescription(text: "home.text.text-3".tr()),
+              const SizedBox(height: 30),
+              TextDescription(text: "home.text.text-4".tr()),
               const SizedBox(height: 50),
-              const TextDescription(
-                  text:
-                      "I was graduated as a Game Developer. In the earliest moment, My team and I form a small local software business which bring our life a real experience dealing a contract. And now I'm became a contractor with Thai Parliament."),
-              const SizedBox(height: 30),
-              const TextDescription(
-                  text:
-                      "Working here involve me to develop skills further to the web era. I was met with Flutter since it was an early stage which is awkward. But keeping momentum in learning and found that it can perform a multiplatform framework by a single codebase blessing my mind. And now I'm become a Flutter (addict) Lover."),
-              const SizedBox(height: 30),
-              const TextDescription(
-                  text:
-                      "Outside work, I enjoy a richful story Genshin Impact with a good cup of coffee. Sometimes listening to an anime piano song is an awesome selection as well."),
+              const Testimonial(path: 'assets/images/galleries/'),
               const SizedBox(height: 30),
               const Footer(),
             ],
